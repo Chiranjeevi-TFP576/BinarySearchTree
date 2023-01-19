12e4435a0c83e3bab9618cd4419ee08230180aa0
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-19 12:40:34 +0530

    [chiru] CreateAnd AddingElements To BinarySearchTree

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Binary SearchTree");
+            BinarySearch<int> binarySearchTree = new BinarySearch<int>(56);
+            binarySearchTree.AddNode(30);
+            binarySearchTree.AddNode(70);         
+            binarySearchTree.Display();
+        }
+    }
+}
